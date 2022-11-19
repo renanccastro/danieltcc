ee90567dc8317c5163209ee15cdadb3b0147266b
Danielly Primo <60229135+dpr1mo@users.noreply.github.com>
2022-11-19 19:07:44 -0300

    feat: Add rotas

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
-from models import Artigos
+from models import Artigos, Topicos
 import json
 
 app = Flask(__name__)
@@ -36,8 +36,50 @@ class Artigo(Resource):
             'conteudo': artigo.conteudo
         }
         return response
-#14min
+
+    def delete(self, titulo):
+        artigo = Artigos.query.filter_by(titulo=titulo).first()
+        mensagem = 'Artigo {} excluído com sucesso'.format(artigo.titulo)
+        artigo.delete()
+        return {'status':'sucesso', 'mensagem': mensagem}
+
+class ListaArtigos(Resource):
+    def get(self):
+        artigos = Artigos.query.all()
+        response = [{'id': i.id, 'titulo': i.titulo, 'conteudo': i.conteudo} for i in artigos]
+        return response
+
+    def post(self):
+        dados = request.json
+        artigo = Artigos(titulo=dados['titulo'], conteudo=dados['conteudo'])
+        artigo.save()
+        response = {
+            'id':artigo.id,
+            'titulo':artigo.titulo,
+            'conteudo':artigo.conteudo
+        }
+        return response
+
+class ListaTopicos(Resource):
+    def get(self):
+        topicos = Topicos.query.all()
+        response = [{'id': i.id, 'titulo': i.titulo} for i in topicos]
+        return response
+
+    def post(self):
+        dados = request.json
+        artigo = Artigos.query.filter_by(titulo=dados['artigo']).first()
+        topico = Topicos(titulo=dados['titulo'], artigo=artigo)
+        topico.save()
+        response = {
+            'titulo': topico.titulo.artigo,
+            'id': topico.id
+        }
+        return response
+
 api.add_resource(Artigo, '/artigo/<string:titulo>/')
+api.add_resource(ListaArtigos, '/artigo/')
+api.add_resource(ListaTopicos, '/topicos/')
 
 if __name__ == '__main__':
     app.run(debug=True)
