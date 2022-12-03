13621731cdf82a45146d601a4443ac58863038f1
Renan Castro <renan@blastradius.ai>
2022-12-03 18:28:39 -0300

    add gh actions deploy and static files

--- Dockerfile
+++ Dockerfile
@@ -6,5 +6,6 @@ WORKDIR /app
 ADD . /app
 RUN apk add --no-cache --virtual=.build-deps \
   git openssh-client build-base mariadb-dev sqlite-libs sqlite-dev sqlite && pip install -r requirements.txt
-
+HEALTHCHECK --interval=5m --timeout=3s \
+  CMD curl -f http://0.0.0.0:9002/api/blog/posts/ || exit 1
 ENTRYPOINT [ "python", "main.py" ]
--- dev_api/app.py
+++ dev_api/app.py
@@ -15,7 +15,7 @@ log = logging.getLogger(__name__)
 
 
 def configure_app(flask_app):
-    flask_app.config['SERVER_NAME'] = settings.FLASK_SERVER_NAME
+    # flask_app.config['SERVER_NAME'] = settings.FLASK_SERVER_NAME
     flask_app.config['SQLALCHEMY_DATABASE_URI'] = settings.SQLALCHEMY_DATABASE_URI
     flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = settings.SQLALCHEMY_TRACK_MODIFICATIONS
     flask_app.config['SWAGGER_UI_DOC_EXPANSION'] = settings.RESTPLUS_SWAGGER_UI_DOC_EXPANSION
@@ -28,7 +28,7 @@ def configure_app(flask_app):
 
 def main():
     configure_app(app)
-    log.info('>>>>> Starting development server at http://{}/api/ <<<<<'.format(app.config['SERVER_NAME']))
+    # log.info('>>>>> Starting development server at http://{}/api/ <<<<<'.format(app.config['SERVER_NAME']))
     blueprint = Blueprint('api', __name__, url_prefix='/api')
     api.init_app(blueprint)
     api.add_namespace(blog_posts_namespace)
